444ffed65e5d3117d8f5959dd1712e49e764e37a
Matthew McCormack <mmccormack@Matthews-MacBook-Pro.local>
2025-03-05 19:09:18 -0500

    quick and dirty UI with audio processing

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('hello world')
+from transformers import pipeline
+import gradio as gr
+import numpy as np
+
+transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-base.en")
+
+def transcribe(audio):
+    sr, y = audio
+    
+    # Convert to mono if stereo
+    if y.ndim > 1:
+        y = y.mean(axis=1)
+        
+    y = y.astype(np.float32)
+    y /= np.max(np.abs(y))
+
+    return transcriber({"sampling_rate": sr, "raw": y})["text"]  
+
+demo = gr.Interface(
+    transcribe,
+    gr.Audio(sources="microphone"),
+    "text",
+)
+
+demo.launch()
